5d7d6414936999b75704f1828b76ffc131440b61
LUIS-HZR <cleveroficial71@gmail.com>
2024-09-13 16:52:48 -0600

    Avance

---
+++ curso.py
@@ -0,0 +1,14 @@
+class Estudiante:
+    nombre_curso = ""
+    instructor = ""
+    codigo_curso = 0
+
+    def __init__(self, nombre_curso, instructor, codigo_curso):
+        self.nombre_curso = nombre_curso
+        self.instructor = instructor
+        self.codigo_curso = codigo_curso
+        
+
+    def mostrar_info_curso(self):
+        pass
+
---
+++ estudiante.py
@@ -0,0 +1,17 @@
+class Estudiante:
+    nombre = ""
+    edad = 0
+    id_estudiante = 0
+    cursos = 0
+
+    def __init__(self, nombre, edad, id_estudiante, cursos):
+        self.nombre = nombre
+        self.edad = edad
+        self.id_estudiante = id_estudiante
+        self.cursos = cursos
+
+    def agregar_curso(self):
+        pass
+    
+    def mostrar_informacion(self):
+        pass
